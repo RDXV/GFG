18813fcd4bb8270a083b6aef839547f518605880
RDXV <varunarora1408@gmail.com>
2021-12-11 12:21:07 +0530

    template ll for push, del, reverse, print

---
+++ template_list.cpp
@@ -0,0 +1,113 @@
+// template for list insertion, deletion and reversal
+
+#include "iostream"
+using namespace std;
+
+// node definition which can be initialized through objects
+class node
+{
+public:
+  int inf;
+  node *nex;
+};
+
+node *push_start(node *head, int data) // insert at head
+{
+  node *temp = new node;
+  temp->inf = data;
+  temp->nex = head;
+
+  return temp;
+}
+
+void push_end(node **head_node, int data) // insert at end
+{
+  node *temp = new node;
+  node *last = *head_node;
+  temp->inf = data;
+  temp->nex = nullptr;
+  if (*head_node == nullptr)
+  {
+    *head_node = temp;
+    return;
+  }
+  while (last->nex != nullptr)
+  {
+    last = last->nex;
+  }
+
+  last->nex = temp;
+  return;
+}
+
+void show(node *h) // print the list
+{
+  if (h == nullptr)
+  {
+    cout << "the list is empty";
+  }
+  else
+  {
+    while (h != nullptr)
+    {
+      cout << h->inf << " ";
+      h = h->nex;
+    }
+  }
+}
+
+node *reverse(node *head) // reverse a list
+{
+  struct node *curr, *next, *prev;
+  curr = head;
+  prev = nullptr;
+  while (curr != nullptr)
+  {
+    next = curr->nex;
+    curr->nex = prev;
+
+    prev = curr;
+    curr = next;
+  }
+  head = prev;
+  return head;
+
+  // head = reverse(head); as the function in main()
+}
+
+node *del(node *start) // delete node at head
+{
+  if (start == nullptr)
+  {
+    cout << "Empty linked list so nothing can be deleted";
+    exit(0);
+  }
+  else if (start->nex == nullptr)
+  {
+    cout << "element deleted. Now the list is empty";
+    start = nullptr;
+  }
+  else
+  {
+    node *temp = new node;
+    temp = start;
+    start = start->nex;
+    delete temp;
+  }
+  return start;
+}
+
+int main()
+{
+  node *head = nullptr;
+  head = push_start(head, 2);
+  head = push_start(head, 3);
+  head = push_start(head, 4);
+  push_end(&head, 1);
+  cout << "\n";
+  show(head);
+  head = reverse(head);
+  cout << "\n";
+  show(head);
+  return 0;
+}
