cbefb0a5721b904dea1038cc7233824626208bf6
RDXV <varunarora1408@gmail.com>
2021-12-11 22:18:38 +0530

    insert in sorted circular list

---
+++ insert_sorted_circular.cpp
@@ -0,0 +1,89 @@
+// insert element in sorted circular
+#include "iostream"
+using namespace std;
+
+class node
+{
+public:
+  int inf;
+  node *nex;
+};
+
+void push_start(node **head, int data)
+{
+  node *ptr1 = new node;
+  node *temp = *head;
+  ptr1->inf = data;
+  ptr1->nex = *head;
+
+  if (*head != nullptr)
+  {
+    while (temp->nex != *head)
+    {
+      temp = temp->nex;
+    }
+    temp->nex = ptr1;
+  }
+  else
+  {
+    ptr1->nex = ptr1;
+  }
+  *head = ptr1;
+}
+
+void show(node *head)
+{
+  node *temp = head;
+  if (head == nullptr)
+  {
+    cout << "the list is empty";
+    exit(0);
+  }
+  do
+  {
+    cout << temp->inf << " ";
+    temp = temp->nex;
+  } while (temp != head);
+}
+
+void insert_sorted(node **head, int data)
+{
+  // cout << "0\n";
+  node *next = *head;
+  node *newnode = new node;
+  newnode->inf = data;
+  if (*head == nullptr)
+  {
+    newnode->nex = newnode;
+    *head = newnode;
+  }
+  else
+  {
+    while (next->nex != *head) // going back to the same value
+    {
+      if (next->inf < data && next->nex->inf > data)
+      {
+        // cout << "1\n";
+        node *second_next = next->nex;
+        next->nex = newnode;
+        newnode->nex = second_next;
+      }
+      next = next->nex;
+      // don't forget adding this statement else program runs in forever while loop
+    }
+  }
+}
+
+int main()
+{
+  node *head = nullptr;
+  push_start(&head, 5);
+  push_start(&head, 3);
+  push_start(&head, 2);
+  push_start(&head, 1);
+  show(head);
+  insert_sorted(&head, 4);
+  cout << "\n";
+  show(head);
+  return 0;
+}
